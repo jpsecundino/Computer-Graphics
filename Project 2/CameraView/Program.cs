4b315aee8e960d44de9cafa7e009061b2d2bf858
jpsecundino <jp.secundino@usp.br>
2021-06-24 21:01:57 -0300

    first commit

---
+++ Project 2/CameraView/Program.cs
@@ -0,0 +1,180 @@
+using Silk.NET.Input;
+using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using Silk.NET.Maths;
+using World_3D;
+
+namespace Tutorial
+{
+    class Program
+    {
+        private static IWindow window;
+        private static GL Gl;
+        private static IKeyboard primaryKeyboard;
+
+        private const int Width = 800;
+        private const int Height = 700;
+        private const string modelFilepath = "C:\\Users\\pedro\\Documents\\Aulas\\cg\\T2\\Models\\bear.obj";
+        private const string textureFilepath = "C:\\Users\\pedro\\Documents\\Aulas\\cg\\T2\\Models\\textures\\wizardTowerDiff.png";
+        private static Texture texture;
+        private static World_3D.Shader Shader;
+        private static Mesh mesh;
+
+        //Setup the camera's location, directions, and movement speed
+        private static Vector3 CameraPosition = new Vector3(0.0f, 0.0f, 3.0f);
+        private static Vector3 CameraFront = new Vector3(0.0f, 0.0f, -1.0f);
+        private static Vector3 CameraUp = Vector3.UnitY;
+        private static Vector3 CameraDirection = Vector3.Zero;
+        private static float CameraYaw = -90f;
+        private static float CameraPitch = 0f;
+        private static float CameraZoom = 45f;
+
+        //Used to track change in mouse movement to allow for moving of the Camera
+        private static Vector2 LastMousePosition;
+
+        private static void Main(string[] args)
+        {
+            var options = WindowOptions.Default;
+            options.Size = new Vector2D<int>(800, 600);
+            options.Title = "LearnOpenGL with Silk.NET";
+            window = Window.Create(options);
+
+            window.Load += OnLoad;
+            window.Update += OnUpdate;
+            window.Render += OnRender;
+            window.Closing += OnClose;
+
+            window.Run();
+        }
+
+        private static void OnLoad()
+        {
+            IInputContext input = window.CreateInput();
+            primaryKeyboard = input.Keyboards.FirstOrDefault();
+            if (primaryKeyboard != null)
+            {
+                primaryKeyboard.KeyDown += KeyDown;
+            }
+            for (int i = 0; i < input.Mice.Count; i++)
+            {
+                input.Mice[i].Cursor.CursorMode = CursorMode.Raw;
+                input.Mice[i].MouseMove += OnMouseMove;
+                input.Mice[i].Scroll += OnMouseWheel;
+            }
+
+            Gl = GL.GetApi(window);
+            texture = new Texture(Gl, textureFilepath);
+            Shader = new World_3D.Shader(Gl, "C:\\Users\\pedro\\Documents\\Aulas\\cg\\T2\\Shaders\\shader.vert", "C:\\Users\\pedro\\Documents\\Aulas\\cg\\T2\\Shaders\\shader.frag");
+
+            mesh = new(Gl, modelFilepath, texture);
+
+
+        }
+
+        private static unsafe void OnUpdate(double deltaTime)
+        {
+            var moveSpeed = 10f * (float) deltaTime;
+
+            if (primaryKeyboard.IsKeyPressed(Key.W))
+            {
+                //Move forwards
+                CameraPosition += moveSpeed * CameraFront;
+            }
+            if (primaryKeyboard.IsKeyPressed(Key.S))
+            {
+                //Move backwards
+                CameraPosition -= moveSpeed * CameraFront;
+            }
+            if (primaryKeyboard.IsKeyPressed(Key.A))
+            {
+                //Move left
+                CameraPosition -= Vector3.Normalize(Vector3.Cross(CameraFront, CameraUp)) * moveSpeed;
+            }
+            if (primaryKeyboard.IsKeyPressed(Key.D))
+            {
+                //Move right
+                CameraPosition += Vector3.Normalize(Vector3.Cross(CameraFront, CameraUp)) * moveSpeed;
+            }
+            if (primaryKeyboard.IsKeyPressed(Key.L))
+            {
+                Gl.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
+            }
+            if (primaryKeyboard.IsKeyPressed(Key.P))
+            {
+                Gl.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Fill);
+            }
+        }
+
+        private static unsafe void OnRender(double deltaTime)
+        {
+            Gl.Enable(EnableCap.DepthTest);
+            Gl.Clear((uint) (ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit));
+
+            Shader.Use();
+            Shader.SetUniform("uTexture0", 0);
+
+            //Use elapsed time to convert to radians to allow our cube to rotate over time
+            var difference = (float) (window.Time * 100);
+
+            var model = Matrix4x4.Identity;
+            //var model = Matrix4x4.CreateRotationY(MathHelper.DegreesToRadians(difference)) * Matrix4x4.CreateRotationX(MathHelper.DegreesToRadians(difference));
+            var view = Matrix4x4.CreateLookAt(CameraPosition, CameraPosition + CameraFront, CameraUp);
+            var projection = Matrix4x4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(CameraZoom), Width / Height, 0.1f, 100.0f);
+
+            Shader.SetUniform("uModel", model);
+            Shader.SetUniform("uView", view);
+            Shader.SetUniform("uProjection", projection);
+
+            //We're drawing with just vertices and no indices, and it takes 36 vertices to have a six-sided textured cube
+            //Gl.DrawElements(PrimitiveType.Triangles, IndicesCount, DrawElementsType.UnsignedInt, (void*)0);
+            mesh.Draw(Gl, Shader);
+        }
+
+        private static unsafe void OnMouseMove(IMouse mouse, Vector2 position)
+        {
+            var lookSensitivity = 0.1f;
+            if (LastMousePosition == default) { LastMousePosition = position; }
+            else
+            {
+                var xOffset = (position.X - LastMousePosition.X) * lookSensitivity;
+                var yOffset = (position.Y - LastMousePosition.Y) * lookSensitivity;
+                LastMousePosition = position;
+
+                CameraYaw += xOffset;
+                CameraPitch -= yOffset;
+
+                //We don't want to be able to look behind us by going over our head or under our feet so make sure it stays within these bounds
+                CameraPitch = Math.Clamp(CameraPitch, -89.0f, 89.0f);
+
+                CameraDirection.X = MathF.Cos(MathHelper.DegreesToRadians(CameraYaw)) * MathF.Cos(MathHelper.DegreesToRadians(CameraPitch));
+                CameraDirection.Y = MathF.Sin(MathHelper.DegreesToRadians(CameraPitch));
+                CameraDirection.Z = MathF.Sin(MathHelper.DegreesToRadians(CameraYaw)) * MathF.Cos(MathHelper.DegreesToRadians(CameraPitch));
+                CameraFront = Vector3.Normalize(CameraDirection);
+            }
+        }
+
+        private static unsafe void OnMouseWheel(IMouse mouse, ScrollWheel scrollWheel)
+        {
+            //We don't want to be able to zoom in too close or too far away so clamp to these values
+            CameraZoom = Math.Clamp(CameraZoom - scrollWheel.Y, 1.0f, 45f);
+        }
+
+        private static void OnClose()
+        {
+            Shader.Dispose();
+            texture.Dispose();
+        }
+
+        private static void KeyDown(IKeyboard keyboard, Key key, int arg3)
+        {
+            if (key == Key.Escape)
+            {
+                window.Close();
+            }
+        }
+    }
+}
---
+++ Project 2/Shader.cs
@@ -0,0 +1,105 @@
+﻿using Silk.NET.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+
+//MAE: Esse aqui eh o que eu modifiquei
+
+namespace World_3D
+{
+    public class Shader : IDisposable
+    {
+        private uint _handle;
+        private GL _gl;
+
+        private Dictionary<string, int> _uniforms = new Dictionary<string, int>();
+
+        public Shader(GL gl, string vertexPath, string fragmentPath)
+        {
+            _gl = gl;
+
+            uint vertex = LoadShader(ShaderType.VertexShader, vertexPath);
+            uint fragment = LoadShader(ShaderType.FragmentShader, fragmentPath);
+            _handle = _gl.CreateProgram();
+            _gl.AttachShader(_handle, vertex);
+            _gl.AttachShader(_handle, fragment);
+            _gl.LinkProgram(_handle);
+            _gl.GetProgram(_handle, GLEnum.LinkStatus, out int status);
+            if (status == 0)
+            {
+                throw new Exception($"Program failed to link with error: {_gl.GetProgramInfoLog(_handle)}");
+            }
+            _gl.DetachShader(_handle, vertex);
+            _gl.DetachShader(_handle, fragment);
+            _gl.DeleteShader(vertex);
+            _gl.DeleteShader(fragment);
+        }
+
+        public void Use()
+        {
+            _gl.UseProgram(_handle);
+        }
+
+        public void SetUniform(string name, int value)
+        {
+            _gl.Uniform1(GetUniformLocation(name), value);
+        }
+
+        public void SetUniform(string name, float value)
+        {
+            _gl.Uniform1(GetUniformLocation(name), value);
+        }
+        public void SetUniform(string name, Vector4 value)
+        {
+            _gl.Uniform4(GetUniformLocation(name), value);
+        }
+
+        public void SetUniform(string name, System.Drawing.Color value)
+        {
+            _gl.Uniform4(GetUniformLocation(name), value.R / 255f, value.G / 255f, value.B / 255f, value.A / 255f);
+        }
+
+        public unsafe void SetUniform(string name, Matrix4x4 value)
+        {
+            _gl.UniformMatrix4(GetUniformLocation(name), 1, false, (float*)&value);
+        }
+
+        public void Dispose()
+        {
+            _gl.DeleteProgram(_handle);
+        }
+
+        private uint LoadShader(ShaderType type, string path)
+        {
+            string src = File.ReadAllText(path);
+            uint handle = _gl.CreateShader(type);
+            _gl.ShaderSource(handle, src);
+            _gl.CompileShader(handle);
+            string infoLog = _gl.GetShaderInfoLog(handle);
+            if (!string.IsNullOrWhiteSpace(infoLog))
+            {
+                throw new Exception($"Error compiling shader of type {type}, failed with error {infoLog}");
+            }
+
+            return handle;
+        }
+
+        private int GetUniformLocation(string name)
+        {
+            if (!_uniforms.TryGetValue(name, out int location))
+            {
+                location = _gl.GetUniformLocation(_handle, name);
+                if (location != -1)
+                    _uniforms[name] = location;
+            }
+
+            if (location == -1)
+            {
+                throw new Exception($"{name} uniform not found on shader.");
+            }
+
+            return location;
+        }
+    }
+}
