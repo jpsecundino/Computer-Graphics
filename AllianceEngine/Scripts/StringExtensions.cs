d0e11ec7c010e8b4df96e2ca91cd57c4d8096a01
Tiago Marino <tmsilva11@gmail.com>
2021-07-08 20:29:02 -0300

    Add ToCapital() to String class
    co-authored-by: JP <jp.secundino@usp.br>

---
+++ AllianceEngine/Scripts/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Numerics;
+
+namespace AllianceEngine
+{
+    public static class StringExtensions
+    {
+        public static string ToCapital(this string s) => s[0].ToString().ToUpper() + s[1..];
+    }
+}
